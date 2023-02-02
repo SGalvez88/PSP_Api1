9a020dfe5e041e0222bf04434d05b9c277e184b9
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-02-02 10:07:10 +0100

    segund commit

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 let api = express();
 
-api.get('/test');
+api.get('/test',(req, res) =>{
+    res.send('Conectado al servidor');
+});
+
+api.listen(8080,() =>{console.log('escuchando puerto...');});
+
+//api.listen(6000);
